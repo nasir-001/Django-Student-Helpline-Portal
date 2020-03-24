e8e225c08b6222d48a9f4121fbcc2ddc0db062aa
Nasir Lawal <nasirlawal001@gmail.com>
2020-03-24 17:10:38 +0100

    Edit signup

---
+++ Helpline_Portal/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+"""
+class RegistrationForm(forms.Form):
+	username = forms.CharField(max_length=50, widget=forms.TextInput(atrrs={'placeholder':'username'}))
+	password = forms.CharField(max_length=50)
+	regnumber = forms.CharField(max_length=10)
+	level = forms.CharField(max_length=1)"""
--- Helpline_Portal/models.py
+++ Helpline_Portal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.from .forms import RegistrationForm
+
+
+class RegistrationData(models.Model):
+	username = models.CharField(max_length=50)
+	password = models.CharField(max_length=50)
+	regnumber = models.CharField(max_length=10)
+	level = models.CharField(max_length=1)
+
+
+	def __str__(self):
+		return self.username
